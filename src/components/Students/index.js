58e98a64423bfe6870e77780d35d0092a1113f7a
Suerta <suerta__@hotmail.com>
2020-10-27 21:17:57 +0800

    [Zhang Jindong] feat: should display students

--- src/components/Students/index.js
+++ src/components/Students/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 class Students extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [],
+    };
+  }
+
+  renderStudent = (student) => <li key={student.id}>{`${student.id}. ${student.name}`}</li>;
+
   render() {
-    return <h2>{this.props.title}</h2>;
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        <ul>
+          {this.state.students.map(this.renderStudent)}
+          <li>
+            <button type="button">+ 添加学员</button>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
