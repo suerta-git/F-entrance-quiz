f69ea21414d7dfb57ca690511e9872f2abaded22
Suerta <suerta__@hotmail.com>
2020-10-27 20:04:37 +0800

    [Zhang Jindong] feat: should connect to server

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount = () => {
+    fetch('http://localhost:8080/connection-test', {
+      method: 'GET',
+      mode: 'cors',
+    })
+      .then((res) => {
+        console.log('调用成功，返回状态：', res.status);
+      })
+      .catch((err) => {
+        console.log('调用失败，错误信息：', err.message);
+      });
+  };
+
   render() {
     return (
       <div data-testid="app" className="App">
