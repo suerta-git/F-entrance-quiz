1634b05778bd39417f016dec1d099591918794f9
Suerta <suerta__@hotmail.com>
2020-10-27 20:51:41 +0800

    [Zhang Jindong] refactor: adjust component arguments

--- src/containers/StudentsContainer/index.js
+++ src/containers/StudentsContainer/index.js
@@ -3,18 +3,12 @@ import Groups from '../../components/Groups';
 import Students from '../../components/Students';
 import styles from './index.module.scss';
 
+const GROUP_NUMBER = 6;
 class StudentGroupsContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [],
-    };
-  }
-
   render = () => (
     <div className={styles.container}>
-      <Groups title="分组列表" students={this.state.students} />
-      <Students title="学员列表" students={this.state.students} />
+      <Groups title="分组列表" groupNumber={GROUP_NUMBER} />
+      <Students title="学员列表" />
     </div>
   );
 }
