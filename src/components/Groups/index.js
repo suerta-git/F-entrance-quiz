f88bf2b5a6e24deaba27900fd0fa3bd62aa5e0a6
Suerta <suerta__@hotmail.com>
2020-10-27 21:09:07 +0800

    [Zhang Jindong] feat: should display groups

--- src/components/Groups/index.js
+++ src/components/Groups/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
 class Groups extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: [],
+    };
+  }
+
+  renderGroup = (group) => (
+    <li key={group.name}>
+      <div>{group.name}</div>
+      <div>
+        <ul>
+          {group.students.map((student) => (
+            <li key={student.id}>{`${student.id}. ${student.name}`}</li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+
   render() {
-    return <h2>{this.props.title}</h2>;
+    return (
+      <div>
+        <nav>
+          <h2>{this.props.title}</h2>
+          <button type="button">分组学员</button>
+        </nav>
+        <ul>{this.state.groups.map(this.renderGroup)}</ul>
+      </div>
+    );
   }
 }
 
